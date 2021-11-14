574030f9fbe9ccf84e6aadce21e092fe77060f3a
felicasaravilla <felipecasaravilla@gmail.com>
2021-11-14 20:04:53 -0300

    Entrega 7 - Carrito
    Actualización en carrito:
    - Validación de datos ingresados por el usuario.
    - Modal para ingresar los medios de pagos.
    - Costo a pagar en total, sumando el costo de envío más el subtotal.

--- js/cart.js
+++ js/cart.js
@@ -36,7 +36,7 @@ function showCarrito(){
 
 }
 
-
+/* modifica el total*/
 function updateTotal(){
     let total = 0;
     let subtotales = document.getElementsByClassName("subtotal");
@@ -45,10 +45,9 @@ function updateTotal(){
     }
   
     document.getElementById("total").innerHTML=total;
-    document.getElementById("totalCost").innerHTML=total;
     document.getElementById("productCost").innerHTML=total;
+    document.getElementById("total").setAttribute("value",total);
 }
-
 
 
 
@@ -61,10 +60,107 @@ function getCarrito(url){
     
 }
 
+/*calculo de el costo de envio*/
+let porcentaje = 0;
+
 document.addEventListener("DOMContentLoaded", function(e){
     getCarrito("https://japdevdep.github.io/ecommerce-api/cart/654.json")
     .then(respuesta=>{
         productosCarrito = respuesta.articles;
         showCarrito();
+        CalculoEnvio();
+        document.getElementById("goldradio").addEventListener("change", function(){
+            porcentaje = 0.05;
+            CalculoEnvio();
+        });
+        
+        document.getElementById("premiumradio").addEventListener("change", function(){
+            porcentaje = 0.07;
+            CalculoEnvio();
+        });
+        document.getElementById("standardradio").addEventListener("change", function(){
+            porcentaje = 0.15;
+            CalculoEnvio();
+        });
     })
 })
+
+
+/*calculo de percontaje de envio*/
+function PorcentajeEnvio(){
+let envios = document.getElementsByName("envio");
+envios.forEach(envio => {
+    if (envio.checked){
+    porcentaje = envio.value;
+    }
+});
+
+let costoenvio = parseFloat(porcentaje);
+return costoenvio
+}
+
+function CalculoEnvio(){
+
+    let resultado = (Math.round(document.getElementById("total").getAttribute("value")*porcentaje));
+
+    document.getElementById("comissionText").innerHTML=resultado;
+    document.getElementById("comissionText").setAttribute("value",resultado);
+    CostoTotal();
+}
+
+/*calculo del total de el precio a pagar*/
+function CostoTotal(){
+    
+    let totalcompras = parseFloat(Math.round(document.getElementById("total").getAttribute("value")));
+    let totalcomision = parseFloat(document.getElementById("comissionText").getAttribute("value"));
+    let costototal = totalcompras + totalcomision
+
+    document.getElementById("totalCost").innerHTML=costototal;
+    document.getElementById("totalCost").setAttribute("value",costototal);
+}
+
+/*validaciones de datos ingresados por el usuario*/
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      let button = document.getElementById('paraenviar');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        button.addEventListener("click", function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+          validarPago()
+        }, false);
+      });
+    }, false);
+  })();
+
+
+  function validarPago(){
+      let numerotarjeta = document.getElementById("numerotarjeta").value;
+      let vencimiento = document.getElementById("vencimiento").value;
+      let cvc = document.getElementById("cvc").value;
+      let cuentabancaria = document.getElementById("cuentabancaria").value;
+
+      let pago = document.querySelector('input[name="pago"]:checked');
+
+      if (pago.id == "credito"){
+              if (numerotarjeta == "" || vencimiento == "" || cvc == ""){
+                  alert ("Ingresar los campos vacios");
+              }else{
+                  alert("Datos de pago ingresados correctamente");
+              }
+      }else{
+          if(cuentabancaria == ""){
+            alert ("Ingresar los campos vacios");
+          }
+          else{
+            alert("Datos de pago ingresados correctamente");
+          }  
+      }
+ }
